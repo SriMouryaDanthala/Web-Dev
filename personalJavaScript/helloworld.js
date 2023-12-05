947e73c2cf1aa8dfd6391b9cd1e972bb91ce2c97
Sri Mourya Danthala <danthalasrimourya@gmail.com>
2023-12-05 14:28:07 +0530

    personalfile

---
+++ personalJavaScript/helloworld.js
@@ -0,0 +1,19 @@
+// alert("Hello world");
+console.log("Hello world is printed on to the console");
+
+let x = 100;
+// here x = 100;
+{
+    let x = 200;
+    let y = 20;
+    // here x = 200;
+    // here y = 20;
+}
+
+console.log(typeof x);
+
+let sp = 222;
+let cp = 133;
+console.log(parseInt(sp/cp));
+// here x = 100; 
+// here y cannot be accessed
